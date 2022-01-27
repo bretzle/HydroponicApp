cf57cb85d29b9c960bdc1b44e0041c47790f26ca
becknellj <jadebecknell@gmail.com>
2022-01-27 08:02:25 -0600

    updated test methods

--- app/src/main/java/edu/hydroponicapp/MainActivity.java
+++ app/src/main/java/edu/hydroponicapp/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.hydroponicapp;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +22,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     final String db_url ="https://hydroponicsapp-7ca52-default-rtdb.firebaseio.com/";
-    FirebaseDatabase urmom = FirebaseDatabase.getInstance(db_url);
-    DatabaseReference dbRef = urmom.getReference("s");
+    FirebaseDatabase database = FirebaseDatabase.getInstance(db_url);
+    DatabaseReference dbRef = database.getReference("sensorValues");
+    Timestamp timestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +44,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                timestamp = new Timestamp(System.currentTimeMillis());
+                //testWrite(dbRef, timestamp);
 
-                dbRef.child("ph3").push().setValue("2");
-
+                //testRead(dbRef);
                 Snackbar.make(view, dbRef.toString(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -61,7 +68,24 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
-
+    private void testWrite(DatabaseReference dbRef, Timestamp time){
+        DatabaseReference unit = dbRef.child("unit_name").child("Durant");
+        unit.child("ph").setValue("2");
+        unit.child("time").setValue(time.toString());
+    }
+    private void testRead(DatabaseReference dbRef){
+        dbRef.child("unit_name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
